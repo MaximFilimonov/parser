fe5dbbe2aa2548c7a1256404716e8fbc0125050a
Maksim_Filimonov <filimonov.m@selectel.ru>
2022-06-15 21:21:16 +0300

    Update license & init commit for get_objects parser

---
+++ get_objects/main.py
@@ -0,0 +1,19 @@
+import fire
+from bs4 import *
+from tqdm import tqdm
+from time import sleep
+import urllib.request, urllib.error, urllib.parse
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    fire.Fire(get_urls)
